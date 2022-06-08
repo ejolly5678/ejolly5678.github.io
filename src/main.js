06262d140cae9e2758824d1a89c6a19384e28d99
E Jolly <39107604+ejolly5678@users.noreply.github.com>
2022-06-08 19:30:08 -0400

    change linking

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-'use strict';
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
-
-counter();
+//sidebar functionalities 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+  
